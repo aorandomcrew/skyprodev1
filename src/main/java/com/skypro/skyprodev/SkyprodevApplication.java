45b49847588258c4b9983098688a51761da0444e
danilbogomolov <skyprodev1769@gmail.com>
2024-04-24 12:46:47 +0300

    Initial commit

---
+++ src/main/java/com/skypro/skyprodev/SkyprodevApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.skyprodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyprodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyprodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/skyprodev/SkyprodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.skyprodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyprodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
